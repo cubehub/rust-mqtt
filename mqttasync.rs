a9f55059f17fed47e0ec5e86a01f57667313b356
Andres Vahter <andres.vahter@gmail.com>
2015-06-11 15:12:23 +0300

    implement connect functions

--- mqttasync.rs
+++ mqttasync.rs
@@ -6,11 +6,21 @@ use libc::{c_char, c_void};
 use std::ffi::CString;
 use std::ptr;
 
-#[allow(non_camel_case_types)]
-pub enum PersistentType {
-    PERSISTENCE_DEFAULT = 0,
-    PERSISTENCE_NONE = 1,
-    PERSISTENCE_USER = 2,
+
+pub enum PersistenceType {
+    Default = 0,
+    Nothing = 1,
+    User = 2,
+}
+
+pub enum ConnectReturnCode {
+    Success = 0,
+    UnacceptableProtocol = 1,
+    IdentifierRejected = 2,
+    ServerUnavailable = 3,
+    BadUsernameOrPassword = 4,
+    NotAuthorized = 5,
+    Reserved = 6,
 }
 
 pub struct AsyncClient {
@@ -19,28 +29,32 @@ pub struct AsyncClient {
 
 impl AsyncClient {
 
-    pub fn new(url: &str, clientid: &str, persistence_type: PersistentType) -> ffimqttasync::MQTTAsync {
-        let mut client: ffimqttasync::MQTTAsync = unsafe{mem::zeroed()};
+    pub fn new(address: &str, clientid: &str, persistence: PersistenceType) -> AsyncClient {
+        let mut fficlient: ffimqttasync::MQTTAsync = unsafe{mem::zeroed()};
         let mut persistence_context: c_void = unsafe{mem::zeroed()};
 
-        let c_url = CString::new(url).unwrap();
+        let c_url = CString::new(address).unwrap();
         let c_clientid = CString::new(clientid).unwrap();
 
         let array_url = c_url.as_bytes_with_nul();
         let array_clientid = c_clientid.as_bytes_with_nul();
 
         unsafe {
-            ffimqttasync::MQTTAsync_create(&mut client,
+            ffimqttasync::MQTTAsync_create(&mut fficlient,
                                            mem::transmute::<&u8, *const c_char>(&array_url[0]),
                                            mem::transmute::<&u8, *const c_char>(&array_clientid[0]),
-                                           persistence_type as i32,
+                                           persistence as i32,
                                            &mut persistence_context);
         }
+
+        let client = AsyncClient {
+            client: fficlient,
+        };
 
         client
     }
 
-    pub fn connect(&mut self) {
+    pub fn connect(&mut self, options: &mut AsyncConnectOptions) -> ConnectReturnCode {
         unsafe {
             ffimqttasync::MQTTAsync_setCallbacks(self.client,
                                                  ptr::null_mut(),
@@ -49,6 +63,40 @@ impl AsyncClient {
                                                  ptr::null_mut(),
                                                );
         }
+
+        // fill in FFI private struct
+        options.options.keepAliveInterval = options.keep_alive_interval;
+        options.options.cleansession = options.cleansession;
+        options.options.maxInflight = options.max_in_flight;
+        options.options.connectTimeout = options.connect_timeout;
+        options.options.retryInterval = options.retry_interval;
+
+        // register callbacks
+        options.options.onSuccess = &mut Some(Self::connect_succeeded);
+        options.options.onFailure = &mut Some(Self::connect_failed);
+
+        let mut error = 0;
+        unsafe {
+            error = ffimqttasync::MQTTAsync_connect(self.client, &options.options);
+        }
+
+        match error {
+            0 => ConnectReturnCode::Success,
+            1 => ConnectReturnCode::UnacceptableProtocol,
+            2 => ConnectReturnCode::IdentifierRejected,
+            3 => ConnectReturnCode::ServerUnavailable,
+            4 => ConnectReturnCode::BadUsernameOrPassword,
+            5 => ConnectReturnCode::NotAuthorized,
+            _ => ConnectReturnCode::Reserved,
+        }
+    }
+
+    extern "C" fn connect_succeeded(context: *const ::libc::c_void, response: *const ffimqttasync::MQTTAsync_successData) {
+        println!("connect succeeded");
+    }
+
+    extern "C" fn connect_failed(context: *const ::libc::c_void, response: *const ffimqttasync::MQTTAsync_failureData) {
+        println!("connect failed");
     }
 
     extern "C" fn disconnected(context: *const c_void, cause: *const c_char) {
@@ -61,13 +109,19 @@ impl AsyncClient {
 }
 
 pub struct AsyncConnectOptions {
-    pub options: ffimqttasync::MQTTAsync_connectOptions,
+    options: ffimqttasync::MQTTAsync_connectOptions,
+
+    pub keep_alive_interval: i32,
+    pub cleansession: i32,
+    pub max_in_flight: i32,
+    pub connect_timeout: i32,
+    pub retry_interval: i32,
 }
 
 impl AsyncConnectOptions {
 
-    pub fn new() -> ffimqttasync::MQTTAsync_connectOptions {
-        let options = ffimqttasync::MQTTAsync_connectOptions {
+    pub fn new() -> AsyncConnectOptions {
+        let ffioptions = ffimqttasync::MQTTAsync_connectOptions {
             struct_id: ['M' as i8, 'Q' as i8, 'T' as i8, 'C' as i8],
             struct_version: 3,
             keepAliveInterval: 60,
@@ -87,12 +141,22 @@ impl AsyncConnectOptions {
             MQTTVersion: 0,
         };
 
+        let options = AsyncConnectOptions {
+            options: ffioptions,
+
+            keep_alive_interval: 20,
+            cleansession: 1,
+            max_in_flight: 10,
+            connect_timeout: 30,
+            retry_interval: 0,
+        };
+
         options
     }
 }
 
 pub struct AsyncDisconnectOptions {
-    pub options: ffimqttasync::MQTTAsync_disconnectOptions,
+    options: ffimqttasync::MQTTAsync_disconnectOptions,
 }
 
 impl AsyncDisconnectOptions {
